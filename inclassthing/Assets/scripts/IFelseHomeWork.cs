a9160fb96829b672bc98e31dfde97c2ec06013df
Unknown <jtenjt6@gmail.com>
2018-10-15 15:36:10 -0600

    If Else Statement homework code

---
+++ inclassthing/Assets/scripts/IFelseHomeWork.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IFelseHomeWork : MonoBehaviour {
+	public string Season;
+
+	// Use this for initialization
+	void Start () {
+		if(Season == "Winter")
+			print("Season is"+ Season);
+		else if(Season == "Summer")
+			print("Season is"+ Season);
+		else if(Season == "Spring")
+			print("Season is"+ Season);
+		else if(Season == "Fall")
+			print("Season is"+ Season);
+		else {
+			print("Season is"+ Season);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
