bb6ac1e43125c65f2fad008b492cac29271ef490
Unknown <jtenjt6@gmail.com>
2018-10-15 17:18:07 -0600

    Created Enemy
    applied scripts to enemy

--- inclassthing/Assets/scripts/EnemyPatrol.cs
+++ inclassthing/Assets/scripts/EnemyPatrol.cs
@@ -8,16 +8,21 @@ public class EnemyPatrol : MonoBehaviour {
 	public float MoveSpeed;
 	public bool MoveRight;
 
+
+
 	//Wall Check
-	public Transform Wallcheck;
+	public Transform WallCheck;
 	public float WallCheckRadius;
 	public LayerMask WhatIsWall;
 	private bool HittingWall;
 
+//Transform is a class type that takes into consideration the character position, rotation, etc. so that the character 
+//when it hits a wall will turn around and walk the other way. 
+
 	//Edge Check
 	private bool NotAtEdge;
 	public Transform EdgeCheck;
-	
+	//This Edge Check code will check to see if enemy is at edge or not at edge, which is why its using a bool 
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +31,26 @@ public class EnemyPatrol : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+
+//use if statements when you need to make a decision
+// || is a logical operator, it means OR so its saying we've hit a wall OR we are at edge, change to opposite direction
+// ! is false, so moveright and !moveright is move right true and moveright false
+		//turn around at wall or edge code
+		if (HittingWall || !NotAtEdge){
+			MoveRight = !MoveRight;
+		}
+
+		if (MoveRight){
+			transform.localScale = new Vector3(-5f,5f,1f);//this is scale transforms on x y and z so enemy doesnt shrink
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		else {
+			transform.localScale = new Vector3(5f,5f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		}
 	}
-}
+
