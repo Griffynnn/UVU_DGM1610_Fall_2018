40e539b850ab709a25cf12558ab3e43db90395af
Unknown <jtenjt6@gmail.com>
2018-10-15 16:10:12 -0600

    More organization
    made folder for homework scripts and moved them to that folder

---
+++ inclassthing/Assets/scripts/EnemyPatrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour {
+
+	//MovementVariables
+	public float MoveSpeed;
+	public bool MoveRight;
+
+	//Wall Check
+	public Transform Wallcheck;
+	public float WallCheckRadius;
+	public LayerMask WhatIsWall;
+	private bool HittingWall;
+
+	//Edge Check
+	private bool NotAtEdge;
+	public Transform EdgeCheck;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
