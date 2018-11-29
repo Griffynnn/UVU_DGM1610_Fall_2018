843784baa2d5250fb474e28e7c1834cc54a122d8
Unknown <jtenjt6@gmail.com>
2018-11-28 17:27:22 -0700

    InClassStuff
    connected main menu to first level, added you win! text when certain score is reached

--- inclassthing/Assets/Sprites/scripts/ImportantScripts/ManagerScripts/AmmoManager.cs
+++ inclassthing/Assets/Sprites/scripts/ImportantScripts/ManagerScripts/AmmoManager.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class AmmoManager : MonoBehaviour {
 
-	 	public int Ammo = 0;
-		public int maxAmmo = 5;
+	 	public int Ammo = 5;
 		public int ammoToAdd = 1;
+		public GameObject Player;
 
  		public Text AmmoText;
-
-	public void AddAmmo (){
-		if (Ammo < 0)
+	void Update(){
+		if (Ammo <= 0){
 			Ammo = 0;
+			Player.GetComponent<PlayerShoot>().enabled = false;
+		}
+		if (Ammo >= 10){
+			Ammo = 10;
+		}
+	}
+	public void AddAmmo (){
+	
+		if (Ammo >= 10){
+			Ammo = 10;
+		}
 		Ammo += ammoToAdd;
 		AmmoText.text = " " + Ammo;
 		print(Ammo);
 	}
+
+
 
 }
 
--- inclassthing/Assets/Sprites/scripts/ImportantScripts/ManagerScripts/ScoreManager.cs
+++ inclassthing/Assets/Sprites/scripts/ImportantScripts/ManagerScripts/ScoreManager.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour {
 
  	public static int Score;
-
+	public int WinScore;
  	Text ScoreText;
+	public Text WinText;
 
+
+void Awake(){
+	Time.timeScale = 1;
+}
 	// Use this for initialization
 	void Start () {
 		ScoreText = GetComponent<Text>();
 
 		Score = 0;
 		
+		WinText.GetComponent<Text>().enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -21,7 +28,16 @@ public class ScoreManager : MonoBehaviour {
 		if (Score < 0)
 			Score = 0;
 		ScoreText.text = " " + Score;
+	
+		if(Score >= WinScore){
+		print("Win Score Reached = " + Score);
+		WinText.GetComponent<Text>().enabled = true;
+		Time.timeScale = 0;
 	}
+		if(Input.GetKeyDown(KeyCode.Escape)){
+		SceneManager.LoadScene(0);
+	}
+}
 
 	public static void AddPoints (int PointsToAdd){
 		Score += PointsToAdd;
