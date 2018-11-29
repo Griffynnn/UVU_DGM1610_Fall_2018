1974901e418f8b6407b46ffd86a90032ca035914
Unknown <jtenjt6@gmail.com>
2018-11-29 14:11:51 -0700

    Arrays
    added new script with arrays, trying to make lootbox that will drop a random item(either heath, ammo, or coin)

---
+++ inclassthing/Assets/Sprites/scripts/ImportantScripts/LootBox.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootBox : MonoBehaviour {
+
+	public GameObject HealthDrop;
+	public GameObject AmmoDrop;
+	public GameObject CoinDrop;
+
+	public int[] LootCrate = new int[3];
+
+	public string[] Loot;
+
+	// Use this for initialization
+	void Start () {
+
+			LootCrate[0] = 0;
+			LootCrate[1] = 1;
+			LootCrate[2] = 2;
+			LootCrate[3] = 3;
+
+			print(LootCrate[2]);
+
+
+			Loot = new string[3];
+
+			Loot[0] = "Nothing";
+			Loot[1] = "a Coin!";
+			Loot[2] = "a life!";
+			Loot[3] = "Ammo!";
+
+			foreach(string item in Loot){
+				print("You Got" + item);
+		
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
